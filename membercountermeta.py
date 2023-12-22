a70e43e9fb8361da3f60e23ddae87a629de5f00b
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-12-22 20:50:53 +0530

    Update membercountermeta.py

--- membercountermeta.py
+++ membercountermeta.py
@@ -9,6 +9,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 import asyncio
+from datetime import datetime
+import pytz
 from texts.texts_teletips import *
 
 MemberCounterMeta = Client(
@@ -24,21 +26,28 @@ MESSAGE_ID = int(os.environ["MESSAGE_ID"])
 print(text_1)
 async def main_MemberCounterMeta():
     async with MemberCounterMeta:
+        # Specify the Indian Standard Time (IST) timezone
+        desired_timezone = 'Asia/Kolkata'
+
+    # Get the current time in the specified timezone
+        current_time = datetime.now(pytz.timezone(desired_timezone)).strftime("%Y-%m-%d %H:%M:%S")
+
         try:
             while True:
                 print(text_2)
-                edit_message_text_teletips = "**📈 | Real-Time Member Counter** [ᵐᵉᵗᵃ](https://github.com/teletips/MemberCounterMeta#:~:text=Versions-,1.0%20Meta,-Credits)"
+                edit_message_text_teletips = "**📈 | Real-Time Member Counter** [Powered By Nihar](t.me/ryuk_xy)"
+                
                 for CHANNEL_OR_GROUP in CHANNEL_OR_GROUP_LIST:
                     try:
                         get_chat_teletips = await MemberCounterMeta.get_chat(int(CHANNEL_OR_GROUP))   
                         if get_chat_teletips.type == "channel":
-                            edit_message_text_teletips += f"\n\n📣  **{get_chat_teletips.title}**\n👤 ├ <i>{get_chat_teletips.members_count} Subscribers</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i>"
+                            edit_message_text_teletips += f"\n\n📣  **{get_chat_teletips.title} 📊**\n👤 ├ <i>{get_chat_teletips.members_count} Subscribers</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i>"
                         else:
-                            edit_message_text_teletips += f"\n\n💬  **{get_chat_teletips.title}**\n👤 ├ <i>{get_chat_teletips.members_count} Members</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i>" 
+                            edit_message_text_teletips += f"\n\n💬  **{get_chat_teletips.title} 📊**\n👤 ├ <i>{get_chat_teletips.members_count} Members</i>\n🔗 └ <i>[Link]({get_chat_teletips.invite_link})</i>" 
                         await asyncio.sleep(2)
                     except ValueError:
                         print(f'ID not found: {CHANNEL_OR_GROUP }. Skipping...')                       
-                edit_message_text_teletips += f"\n\n<i>Automatically refreshes every 15 minutes</i>"
+                edit_message_text_teletips += f"\n\n<i>🌀 Automatically refreshes every 15 minutes \n\n ♻️ **Last Refreshed** : {current_time} ({desired_timezone}) </i>"
                 try:
                     await MemberCounterMeta.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, edit_message_text_teletips, disable_web_page_preview=True)
                 except Exception:
